3fccaaf46645b911809ec36a74ea87ded0d82490
Isaac Oh <isaacohishere@gmail.com>
2016-03-14 22:34:52 -0700

    Initial commit

---
+++ platforms/android/assets/www/js/checkFirstRiddle.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$("#submitButton").on("click", checkRiddle1Answer);
+
+	function checkRiddle1Answer (){
+		var riddle1input = $("#riddle1input").val();
+		var thisInput = riddle1input.toString().toLowerCase(); 
+
+		if (thisInput === ""){
+			return false;
+		}
+		else if (thisInput === "andrew"){
+			$(".wrongButton").addClass("hideWrongButton");
+			$(".correctButton").removeClass("hideCorrectButton");
+			if (!$(".correctButton").hasClass("hideCorrectButton") && !$(".correctButton2").hasClass("hideCorrectButton2")){
+				$(".blackBackground").removeClass("hide");
+				$(".successKey").removeClass("hide");
+				$(".successKey").addClass("animated fadeInDown");
+			}
+			return false;
+		}
+		else {
+			$(".wrongButton").removeClass("hideWrongButton");
+			return false;
+		}
+
+	};
+
+});
---
+++ www/js/checkSecondRiddle.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	var riddle2Counter = 0; 
+	var riddle2Answer = "NSSWE";
+	var riddle2Guess = ""; 
+	$(".arrow-up").on("click", directionCounterUp);
+	$(".arrow-down").on("click", directionCounterDown);
+	$(".arrow-right").on("click", directionCounterRight);
+	$(".arrow-left").on("click", directionCounterLeft);
+
+	$("#submitButton2").on("click", checkRiddle2Guess);
+
+	function directionCounterUp() {
+		riddle2Counter++;
+		riddle2Guess += "N"; 
+	};
+
+	function directionCounterDown() {
+		riddle2Counter++;
+		riddle2Guess += "S";
+	};
+
+	function directionCounterRight() {
+		riddle2Counter++;
+		riddle2Guess += "E";
+	};
+
+	function directionCounterLeft() {
+		riddle2Counter++;
+		riddle2Guess += "W";
+	};
+
+	function checkRiddle2Guess() {
+		if (riddle2Guess === ""){
+			return false;
+		}
+		else if (riddle2Guess === riddle2Answer){
+			$(".wrongButton2").addClass("hideWrongButton2");
+			$(".correctButton2").removeClass("hideCorrectButton2");
+			if (!$(".correctButton").hasClass("hideCorrectButton") && !$(".correctButton2").hasClass("hideCorrectButton2")){
+				$(".blackBackground").removeClass("hide");
+				$(".successKey").removeClass("hide");
+				$(".successKey").addClass("animated fadeInDown");
+			}
+			return false; 
+		}
+		else {
+			$(".wrongButton2").removeClass("hideWrongButton2");
+			riddle2Guess = "";
+			return false;
+		}
+	}
+});
